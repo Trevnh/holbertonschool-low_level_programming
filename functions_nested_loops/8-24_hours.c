98b4d64be7d97800a6719de4151d065b61e1e6de
Trevnh <trevorhuynh@hotmail.com>
2026-02-09 10:01:07 +0000

    Add 8-24_hours.c and update main.h to include jack_bauer()

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day
+ * from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hr / 10));
+			_putchar('0' + (hr % 10));
+			_putchar(58);
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar(10);
+		}
+	}
+}
