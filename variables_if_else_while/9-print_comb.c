e677eb08722a1b1bee2c2bbc7b561e82e0bef8ee
Trevnh <trevorhuynh@hotmail.com>
2026-02-09 07:18:48 +0000

    Add 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ * in ascending order separated by , and followed by
+ * a space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
