c7b4b3efa1edef33211c052169de554e957c808d
Trevnh <trevorhuynh@hotmail.com>
2026-02-09 09:02:21 +0000

    Add 2-print_alphabet_x10.c and update main.h to include print_alphabet_x10()

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet, in lowercase
+ * followed by a new line 10 times.
+ *
+ * Return
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+		_putchar(10);
+	}
+}
