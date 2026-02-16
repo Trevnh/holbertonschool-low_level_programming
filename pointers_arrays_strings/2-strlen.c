2c462310cba5452fb47f7e44989852475ea29c07
Trevnh <trevorhuynh@hotmail.com>
2026-02-16 23:29:33 +0000

    Add 2-strlen.c and update main.h to include _strlen()

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - Get the length of s as an int
+ * and return the value
+ * @s: char pointer
+ *
+ * Return: The length of s
+ */
+int _strlen(char *s)
+{
+	int i, count = 0;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
