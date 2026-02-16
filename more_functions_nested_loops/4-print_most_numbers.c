7998503ea900ddbb6968e1e8ac2caf67c4c945b8
Trevnh <trevorhuynh@hotmail.com>
2026-02-16 08:50:24 +0000

    Add 4-print_most_numbers.c and update main.h to include print_most_numbers()

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print numbers in order from 0 to 9
+ * except 2 and 4
+ *
+ * Return
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if ((i != 50) && (i != 52))
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar(10);
+}
