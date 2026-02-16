63b00e7f64961f4e297a661eabdd5918e30d0f45
Trevnh <trevorhuynh@hotmail.com>
2026-02-16 08:29:39 +0000

    Add 2-mul.c and update main.h to include mul()

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiply a and b and return it
+ * @a:integer
+ * @b:integer
+ *
+ * Return: The result of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
