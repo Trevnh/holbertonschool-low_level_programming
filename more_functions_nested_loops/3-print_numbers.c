44bddccfeedcfc78ca3b9907d76ab591bceb8f5d
Trevnh <trevorhuynh@hotmail.com>
2026-02-16 08:35:51 +0000

    Add 3-print_numbers.c and update main.h to include print_numbers()

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers in order from 0 to 9
+ *
+ * Return
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
+}
