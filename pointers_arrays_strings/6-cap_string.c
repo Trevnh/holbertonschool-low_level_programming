0870cee7be548ab3c507f0228d56b7e2554c783e
Trevnh <trevorhuynh@hotmail.com>
2026-02-17 11:12:12 +0000

    Add 6-cap_string.c and update main.h to include cap_string()

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - Capitalise all words of a string
+ * Separators of words are space, tabulation, new line,
+ * comma, semicolon, period, exclamation mark, question mark,
+ * quotation mark, (, ), { and }.
+ * @s: string
+ *
+ * Return: New string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char sep[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if ((s[i + 1] >= 'a') && (s[i + 1] <= 'z'))
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
