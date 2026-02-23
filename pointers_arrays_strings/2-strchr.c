71fde1e46658c7d4a0eae4eb8567a387c35c3bc9
Trevnh <trevorhuynh@hotmail.com>
2026-02-23 08:17:31 +0000

    Add 2-strchr.c and update main.h to include _strchr()

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - Returns the pointer to the first occurence
+ * of character c in string s, or NULL if the character
+ * is not found
+ * @s: memory area
+ * @c: character
+ *
+ * Return: Returns pointer to first occurence of character c
+ * or NULL if it is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return ('\0');
+}
