3ad502ea6bb1d13345fa75dc0f959f44e80b5a7f
Trevnh <trevorhuynh@hotmail.com>
2026-02-23 23:54:42 +0000

    Add 2-strlen_recursion.c and update main.h to include _strlen_recursion()

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: string
+ *
+ * Return: Length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == '\0')
+	{
+		return (count);
+	}
+	if (*s != '\0')
+	{
+		count++;
+		count += _strlen_recursion(s + count);
+	}
+	return (count);
+}
