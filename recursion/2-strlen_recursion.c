a7a49548bd6da9b7f63d25dd29f9d1405ce84d9d
Trevnh <trevorhuynh@hotmail.com>
2026-02-24 01:22:55 +0000

    Clean up the code and reduce line count

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -7,16 +7,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
 	if (*s == '\0')
 	{
-		return (count);
+		return (0);
 	}
-	if (*s != '\0')
-	{
-		count++;
-		count += _strlen_recursion(s + 1);
-	}
-	return (count);
+	return (1 + _strlen_recursion(s + 1));
 }
