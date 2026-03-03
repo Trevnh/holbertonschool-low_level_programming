eb2c5be4ef03888a01612957bb33492d8c73d23e
Trevnh <trevorhuynh@hotmail.com>
2026-03-03 00:32:26 +0000

    Remove if condition for when both s1 and s2 are NULL

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,10 +19,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (s1 == NULL && s2 == NULL)
-	{
-		return (NULL);
-	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
