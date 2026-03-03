863e005800b595b03ec9b6295d200edef7b6dfeb
Trevnh <trevorhuynh@hotmail.com>
2026-03-03 02:20:16 +0000

    Add 4-new_dog.c and update dog.h to inlcude new_dog()

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - Creates a new dog
+ * @name: pointer to name of dog
+ * @age: float for age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: newly created dog or NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k = 0;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	while (name[i] != '\0')
+	{
+		i++;
+	}
+	while (owner[j] != '\0')
+	{
+		j++;
+	}
+	dog->name = malloc(sizeof(char) * i);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		dog->name[k] = name[k];
+	}
+	dog->name[k] = '\0';
+	dog->age = age;
+	dog->owner = malloc(sizeof(char) * j);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < j; k++)
+	{
+		dog->owner[k] = owner[k];
+	}
+	dog->owner[k] = '\0';
+	return (dog);
+}
