0e757b2f6679791eea085a8cef22c9c0178eac2b
Trevnh <trevorhuynh@hotmail.com>
2026-03-03 00:56:34 +0000

    Add sizeof(char) into malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len2;
 	}
-	arr = malloc(len1 + n + 1);
+	arr = malloc(sizeof(char) * (len1 + n + 1));
 	if (arr == NULL)
 	{
+		free(arr);
 		return (NULL);
 	}
 	for (i = 0; i < len1; i++)
