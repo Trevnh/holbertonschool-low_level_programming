e0d17f071d0ecffd0c350c49adf663b83f329436
Trevnh <trevorhuynh@hotmail.com>
2026-03-03 01:19:46 +0000

    Add dog.h and README.md

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog information
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Dog information
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
