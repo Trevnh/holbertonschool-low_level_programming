a8758c98179eb647782a6b8a154d16dbe02e5c0f
Trevnh <trevorhuynh@hotmail.com>
2026-03-11 05:15:05 +0000

    Add README.md, function_pointers.h and 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - uses name to input into the f function
+ * @name: string of a name
+ * @f: function to print name
+ *
+ * Return:
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * Functions
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
