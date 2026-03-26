4ce8198ed9f8199c1325d1a6b8e4295fcaa535af
Trevnh <trevorhuynh@hotmail.com>
2026-03-26 10:32:19 +0000

    Update lists.h and add skeleton of 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - 
+ * @h: head of linked list
+ * @idx: index to insert node
+ * @n: value to give the node
+ *
+ * Return: Address of new node, NULL if failed or not possible to add at idx
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+
+	return (NULL);
+}
