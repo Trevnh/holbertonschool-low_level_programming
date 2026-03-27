4dcb816dac6fa401e6025a6e26eff273c1d64a24
Trevnh <trevorhuynh@hotmail.com>
2026-03-27 09:55:46 +0000

    Fix exit code for incorrect usage

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(98);
+		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
