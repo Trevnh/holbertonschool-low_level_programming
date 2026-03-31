08a10cb7a67c2e2da554b6b3a1c22455f929773f
Trevnh <trevorhuynh@hotmail.com>
2026-03-31 02:20:24 +0000

    Add 4-hash_table_get.c and update hash_tables.h

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to get value from
+ * @key: key associated with value
+ *
+ * Return: Value or NULL if key is not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *lhead;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+	idx = key_index((const unsigned char *)key, ht->size);
+	lhead = ht->array[idx];
+	while (lhead)
+	{
+		if (!strcmp(lhead->key, key))
+		{
+			return (lhead->value);
+		}
+	}
+	return (NULL);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -48,5 +49,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_node(const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
